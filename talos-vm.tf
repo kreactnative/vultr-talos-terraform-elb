6c8b3cff00f033bfc775ac6a6a9fc4c056724fbe
Narongchai Thongkongkeaw <narongchai.t@socket9.com>
2024-12-30 22:38:22 +0700

    Add patch custom disk vda

--- talos-vm.tf
+++ talos-vm.tf
@@ -1,20 +1,20 @@
 resource "vultr_instance" "control_plane_instance" {
   count       = 2
-  plan        = "vc2-1c-2gb"
+  plan        = "vc2-2c-4gb"
   region      = var.region
   iso_id      = vultr_iso_private.talos_iso.id
-  label       = "talos-control-plane"
-  tags        = ["talos", "control-plane"]
+  label       = "talos-control-plane-${count.index}"
+  tags        = ["talos", "kubernetes", "control-plane"]
   hostname    = "talos-control-plane-${count.index}"
   enable_ipv6 = true
 }
 resource "vultr_instance" "worker_instance" {
-  count       = 2
-  plan        = "vc2-1c-2gb"
+  count       = 1
+  plan        = "vc2-1c-1gb"
   region      = var.region
   iso_id      = vultr_iso_private.talos_iso.id
-  label       = "talos-worker"
-  tags        = ["talos", "worker"]
+  label       = "talos-worker-${count.index}"
+  tags        = ["talos", "kubernetes", "worker"]
   hostname    = "talos-worker-${count.index}"
   enable_ipv6 = true
 }
